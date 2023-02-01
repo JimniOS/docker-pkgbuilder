687bfd1a84d995f2d9ae09352decafebcde35584
wiz64 <67432394+wiz64@users.noreply.github.com>
2023-02-01 14:15:06 +0000

    create dir if not exists

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 # Description: Download PKGBUILDs from GitHub/Repositories
 echo " - neofetch"
+if [ ! -d "packages/neofetch" ]; then
+    mkdir -p packages/neofetch
+fi
 curl -s https://api.github.com/repos/jimnilinux/neofetch/releases/latest | grep "browser_download_url.*PKGBUILD" | cut -d : -f 2,3 | tr -d \" | wget -qi - -O packages/neofetch/PKGBUILD
 echo "-------- DOWNLOAD COMPLETE ---------"
