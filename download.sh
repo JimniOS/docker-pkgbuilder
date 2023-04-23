9c093a935c05015129d1129d69a49e0301bf71e5
wiz64 <67432394+wiz64@users.noreply.github.com>
2023-04-23 14:00:38 +0000

    add saavn-cli

--- download.sh
+++ download.sh
@@ -4,4 +4,10 @@ if [ ! -d "packages/neofetch" ]; then
     mkdir -p packages/neofetch
 fi
 curl -s https://api.github.com/repos/jimnios/neofetch/releases/latest | grep "browser_download_url.*PKGBUILD" | cut -d : -f 2,3 | tr -d \" | wget -qi - -O packages/neofetch/PKGBUILD
+
+echo " - saavn-cli"
+if [ ! -d "packages/saavn-cli" ]; then
+    mkdir -p packages/saavn-cli
+fi
+wget https://raw.githubusercontent.com/wiz64/saavn-cli/main/PKGBUILD -O packages/saavn-cli/PKGBUILD
 echo "-------- DOWNLOAD COMPLETE ---------"
